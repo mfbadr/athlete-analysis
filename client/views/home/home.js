42a217fd4f34046b304e82b232c2e587e5d5a38f
Mikey Badr <mfbadr@gmail.com>
2015-05-05 12:58:22 -0400

    sentiment scores shown

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -7,24 +7,18 @@
 
     $scope.submit = function(){
       $scope.results = 'loading';
-      //alert($scope.athlete);
       Athlete.getResults($scope.athlete).then(function(response){
         //success
         var last = 0,
             resultsArray = response.data.data;
-
-      debugger;
 
         for(var i = 0; i < resultsArray.length; i++){
           if(resultsArray[i].docSentiment && resultsArray[i].docSentiment.score){
             last += parseFloat(resultsArray[i].docSentiment.score);
           }
         }
-        //
 
-        debugger;
         response.data.averageSentiment = last / resultsArray.length;
-        debugger;
         $scope.results = response.data;
       }, function(response){
         //failure
